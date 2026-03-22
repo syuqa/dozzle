fbd3f2c7fab9ddcaf87a8b139131ac0506b22dbb
syuqa <k5.45mm@gamil.com>
2026-03-22 23:29:24 +0300

    fix: wire scan runner into swarm agent server

--- main.go
+++ main.go
@@ -97,7 +97,7 @@ func main() {
 		// Create client service for agent server in swarm mode
 		clientService := docker_support.NewDockerClientService(localClient, args.Filter)
 		// TODO add notification for swarm mode
-		server, err := agent.NewServer(clientService, certs, args.Version(), nil)
+			server, err := agent.NewServer(clientService, certs, args.Version(), nil, trivy.NewScanner(args.TrivyPath))
 		if err != nil {
 			log.Fatal().Err(err).Msg("failed to create agent")
 		}
